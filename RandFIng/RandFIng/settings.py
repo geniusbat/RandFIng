85c8f260a0b7f9bbc2211e17caa63f528a3db049
geniusbat-Tytan <43751753+geniusbat@users.noreply.github.com>
2024-10-01 11:53:53 +0200

    Impromptu changes

--- RandFIng/RandFIng/settings.py
+++ RandFIng/RandFIng/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), os.path.join(BASE_DIR, 'me
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
-MEDIA_URL = '/static/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
